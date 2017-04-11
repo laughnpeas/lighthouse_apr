c3509bc5a99b1fa11122016b5d3d20ac7710d47b
Sean Park <stpnpark@gmail.com>
2017-04-10 17:31:39 -0700

    Step 4: Create the requestURL

--- w2d1/github_avatar_dnldr/download_avatars.js
+++ w2d1/github_avatar_dnldr/download_avatars.js
@@ -1,7 +1,10 @@
 var request = require('request');
+var GITHUB_USER = "laughnpeas";
+var GITHUB_TOKEN = "d5c5e97fadbe28a9675e2ca078f651cbf15bb13e";
+
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
