0f7cc448bec6c0caf1b6c138c3b2889f43a6c354
Sean Park <stpnpark@gmail.com>
2017-04-14 01:30:03 -0700

    User Registration

--- TinyApp/express_server.js
+++ TinyApp/express_server.js
@@ -5,11 +5,13 @@ const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const app = express();
 const PORT = process.env.PORT || 8080;
-const users = require('./users');
+const userData = require('./users');
+const urlData = require('./urls');
 var bcrypt = require('bcrypt');
 
 app.set('view engine', 'ejs');
 // app.use(cookieParser());
+// Using cookie-session
 app.use(cookieSession({
   name: 'session',
   keys: ['lighthouse'],
@@ -18,73 +20,70 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000 // 24 hours 
 }));
 app.use(bodyParser.urlencoded({extended: false}));
-/******* Hard coded Data********************/
+app.locals.urls = urlData;
 
-let urlDatabase = [
-  { id: 1,
-    shortURL: "b2xVn2",
-    longURL: "http://www.lighthouselabs.ca",
-  },
-  { id: 1,
-    shortURL: "b2xVn2",
-    longURL: "http://www.lighthouselabs.ca",
-  },
-  { id: 2 ,
-    shortURL: "9sm5xK",
-    longURL: "http://www.google.com"
-  }
-];
-
-/******* Hard coded Data********************/
-let title = "***Tiny App***";
+//Landing page
 app.get('/', (req, res) => {
   let user = null;
-  // if(req.cookies.user){
-    if(req.session.userId){
-    user = users.find( (user) => {
-      return user.id == req.session.userId;
-    });
-  }  
-  
-  // let urls = getOwnUrl(req.session.userId) || null;
-  res.render('urls_index', {title: title, user: user, urls: urlDatabase});
+    if(req.session.user){
+      user = userData.find( (data) => {
+        return data.id == req.session.user.id;
+      });
+    }  
+  res.render('urls_index', {title: "Tiny App", user: user});
 });
 
 app.get('/login', (req, res, next) => {
-  res.render('login', {title: "Login", err: null});
+  res.render('login', {title: "Login"});
 });
-app.post("/login", (req, res, next) => {
-  let username = req.body.username;
-  let password = req.body.password;
 
-  let user = users.find( (user) => {
-    return (user.username === username)? user.username === username : null;
-  });
+app.post("/login", (req, res, next) => {
+
+  let loginUser = {
+    username: req.body.username,
+    password: req.body.password
+  };
+
   let err = null;
-  if(user === undefined){
+  //Login Error Handling
+  if(!loginUser.username){
     err = new Error('User name does not exists');
-    err.statusCode = 400;
+    err.statusCode = 403;
     return next(err);
-    // res.redirect('/login');
-  }else{
-    bcrypt.compare(password, user.password, function(err, result) {
+  }
+
+  if(!loginUser.password){
+    err = new Error('User password does not exists');
+    err.statusCode = 403;
+    return next(err);
+  }
+
+  let user = userData.find( (data) => {
+    return (data.username === loginUser.username) || null;
+  });
+
+  if(!user){
+    err = new Error('User does not exists');
+    err.statusCode = 403;
+    return next(err);
+  }
+  if(user){
+    bcrypt.compare(loginUser.password, user.password, function(err, result) {
       if (result) {
-        req.session.userId = user.id;
-        // res.cookie('user', user);
+        req.session.user = user;
         res.redirect('/');
-      }else{
+  }else{
         err = new Error('Password does not match');
-        err.statusCode = 400;
+        err.statusCode = 403;
         return next(err);
-        // res.redirect('/login');
       }
     });
   }
 });
 
 app.post("/logout", (req, res) => {
+  //after logout, clear cache with sessing signature
   res.clearCookie('session.sig', { path: '/' });
-  // req.session.destroy();
   res.redirect("/");
 });
 
@@ -92,143 +91,135 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-let userIdArr = users.map( (arr) => {
-  return arr['id'];
-});
-let nextUserId = userIdArr.reduce(function(a, b) {
-    return Math.max(a, b);
-});
-app.post("/register", (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  let email = req.body.email;
+//get maximum user id from user data and generate the next user id
+let nextUserId = userData.map( (user) => { return user['id'];})
+                  .reduce((a, b) => { return Math.max(a, b); });
 
-  let user = users.find( (user) => { return user.email === email; }) || null;
+app.post("/register", (req, res, next) => {
+  let newUser = {
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email
+  };
+
+  //check if provided information do exist in existing data
+  let user = userData.find( (user) => { return user.email === newUser.email; }) || null;
+  let err = new Error();
   if(user){
-    res.status(400).send( `<h4>You are trying to register with existing id</h4> <a href="/register">Go Back</a>` );
+    err.message = 'You are trying to register with existing id';
+    err.statusCode = 400;
+    return next(err);
   }else{
-    if(!username){
-      res.status(400).send( `<h4>You don't provide any user id</h4> <a href="/register">Go Back</a>` );
+    if(!newUser.username){
+      err.message = `You don't provide any user id`;
+      err.statusCode = 400;
+      return next(err)
     }
-    if(!password){
-      res.status(400).send( `<h4>You don't provide any password </h4> <a href="/register">Go Back</a>` );
+    if(!newUser.password){
+      err.message =`You don't provide any password`;
+      err.statusCode = 400;
+      return next(err)
     }
-    if(!email){
-      res.status(400).send( `<h4>You don't provide any email </h4> <a href="/register">Go Back</a>` );
+    if(!newUser.email){
+      err.message =`You don't provide any email`;
+      err.statusCode = 400;
+      return next(err)
     }
-    nextUserId += 1;
-    newUser = {id: nextUserId, username: username, password: password, email: email};
-    if(users.push(newUser)){
-      // res.cookie('user', newUser);
-      req.session.userId = newUser.id;
-    }
-    res.redirect('/');
-  }
+    
+    newUser['id'] = nextUserId += 1;
 
+    if(userData.push(newUser)){
+      req.session.user = newUser;
+      res.redirect('/');
+    }else{
+      err.message =`Failed to register.`;
+      err.statusCode = 400;
+      return next(err)
+    }
+  }
 });
 
 app.get('/urls', (req, res) => {
-  let templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars);
+  res.render("urls_index");
 });
 
-app.get("/urls/new", (req, res) => {
-  // if(req.cookies.user){
-  if(req.session.userId){
+app.get("/urls/new", (req, res, next) => {
+  //allow to add a URL after logging in
+  if(req.session.user){
     res.render("urls_new");
   }else{
-    res.status(400).send( `<h4>You need to login to add a URL</h4> <a href="/">Go Back</a>` );
-    // res.redirect('/');
+    let err = new Error(`You need to login to add a URL.`);
+    err.statusCode = 400;
+    return next(err);
   }
 });
 
-app.get("/", (req, res) => {
-  res.render("");
-});
-
-app.post("/", (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  let email = req.body.email;
-
-  let user = users.find( (user) => { return user.email === email; }) || null;
-  if(user){
-    res.status(400).send( `<h4>You are trying to  with existing id</h4> <a href="/">Go Back</a>` );
-  }else{
-    if(!username){
-      res.status(400).send( `<h4>You don't provide any user id</h4> <a href="/">Go Back</a>` );
-    }
-    if(!password){
-      res.status(400).send( `<h4>You don't provide any password </h4> <a href="/">Go Back</a>` );
-    }
-    if(!email){
-      res.status(400).send( `<h4>You don't provide any email </h4> <a href="/">Go Back</a>` );
-    }
-    nextUserId += 1;
-    newUser = {id: nextUserId, username: username, password: password, email: email};
-    if(users.push(newUser)){
-      res.cookie('user', newUser);
-    }
-    res.redirect('/');
-  }
-
-});
-
-app.post("/urls", (req, res) => {
+app.post("/urls", (req, res, next) => {
   // get longURL from urls_new and put it into the DB
   let longURL = (req.body.longURL.includes("http://")) 
               ? req.body.longURL 
               : "http://"+req.body.longURL; 
               
   let shortURL = getshortURL(longURL);
-  urlDatabase[shortURL] = longURL;
-  let templateVars = {urls: urlDatabase, username: req.cookies["username"]};
-
-  // if inserting data has suceeded go to index with urlDatabase 
-  if(urlDatabase.hasOwnProperty(shortURL)){
-    res.render('urls_index', templateVars );
+  let newUrlEntry = {id: req.session.user.id, shortURL: shortURL, longURL: longURL};
+  if(urlData.push(newUrlEntry)){
+    // if inserting data has suceeded go to index with urlDatabase 
+    let newUrl = urlData.find( (entry) => {
+      entry.shortURL == newUrlEntry.shortURL;
+    });
+    res.redirect('/');
   }else{
   // else send failed message 
-    res.send(`Failed to insert ${shortURL}` );
+    let err = new Error(`Failed to add a URL`);
+    err.statusCode = 400;
+    return next(err);
   }
 });
 
-app.post("/urls/:id/delete", (req, res) => {
-  let id = req.params.id;
-  if(delete urlDatabase[id]){
-    // res.cookie('username', req.cookies['username'] || '');
-    res.render("urls_index", {urls: urlDatabase, username: req.cookies['username'] || ''} );
+app.post("/urls/:id", (req, res, next) => {
+  let shortURL = req.params.id;
+  let longURL = req.body.longURL;
+
+  let newUrlEntry = {id: req.session.userId, shortURL: shortURL, longURL: longURL};
+  if(urlData.push(newUrlEntry)){
+    res.render('urls_index');
   }else{
-    res.send(`Failed to delete ${id}` );
+    let err = new Error(`Failed to update URL ${id}`);
+    err.statusCode = 400;
+    return next(err);
+  }
+});
+
+app.post("/urls/:id/delete", (req, res, next) => {
+  let id = req.params.id;
+  if(delete urlData[id]){
+    res.locals.urls = urlData;
+    res.redirect('/');
+  }else{
+    let err = new Error(`Failed to delete ${id}`);
+    err.statusCode = 400;
+    return next(err);
   };  
 });
 
 app.get("/u/:shortURL", (req, res) => {
 
   let shortURL = req.originalUrl.substr(3);
-  let longURL = urlDatabase[shortURL];
+  let longURL = urlData[shortURL];
   res.redirect(longURL);
 });
 
 app.get("/urls/:id", (req, res) => {
   let shortURL = req.params.id;
-  let ownData = getOwnUrl(req.session.userId);
-  let templateVars = { shortURL:  shortURL, longURL: urlDatabase[shortURL] };
-  res.render("urls_show", templateVars);
+  let selectedUrl = urlData.find( (url)=>{
+    return url.shortURL == shortURL;
+  });
+  res.render("urls_show", { url: selectedUrl });
 });
 
 function getOwnUrl(id){
   return urlDatabase[id];
 }
-app.post("/urls/:id", (req, res) => {
-  let shortURL = req.params.id;
-  let longURL = req.body.longURL;
-  
-  let newUrlEntry = {id: req.session.userId, shortURL: longURL};
-  urlDatabase.push(newUrlEntry);
-  // res.cookie('user', req.cookies['user'] || '');
-  res.redirect("/");
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
@@ -247,8 +238,8 @@ function getshortURL() {
 
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('login', {
-    err: err.message || '',
+  res.render('error', {
+    err: err.message || 'Unknow Error.',
     status: err.statusCode
   });
 });
