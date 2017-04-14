598b57485188616b0b1e4a94eec76df8542a1250
Sean Park <stpnpark@gmail.com>
2017-04-14 01:31:04 -0700

    adding error page and url data

---
+++ TinyApp/urls.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { id: 1,
+    shortURL: "b2xVn2",
+    longURL: "http://www.lighthouselabs.ca",
+  },
+  { id: 2 ,
+    shortURL: "9sm5xK",
+    longURL: "http://www.google.com"
+  }
+];
